67d33a2583ec5e3f999afaed587190ffc27d4243
Samartha S <samarthas729@gmail.com>
2025-09-02 00:07:32 +0530

    Update arjuna_docker_install.sh

--- arjuna_docker_install.sh
+++ arjuna_docker_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+read -s -p "Enter your sudo password: " SUDO_PASSWORD
+echo
 
 echo "[WRAPPER] Running VS Code setup as user..."
-sudo bash ~/arjuna_R2_docker/arjuna_docker_prereq.sh
+echo "$SUDO_PASSWORD" | sudo -S bash ~/arjuna_R2_docker/arjuna_docker_prereq.sh
 
 echo "[WRAPPER] Running docker+GPU setup as root..."
-sudo bash ~/arjuna_R2_docker/arjuna_docker_build.sh
+echo "$SUDO_PASSWORD" | sudo -S bash ~/arjuna_R2_docker/arjuna_docker_build.sh
 
 echo "[WRAPPER] All done ✅"
