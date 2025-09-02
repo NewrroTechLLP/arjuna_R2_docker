0a71796c1b8b59ecb60e7ba07eba26931b63166b
Samartha S <samarthas729@gmail.com>
2025-09-02 21:37:24 +0530

    Update arjuna_docker_install.sh

--- arjuna_docker_install.sh
+++ arjuna_docker_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-read -s -p "Enter your sudo password: " SUDO_PASSWORD
-echo
+# The SUDO_PASSWORD variable is now expected to be passed from the parent script
 
 echo "[WRAPPER] Running VS Code setup as user..."
 echo "$SUDO_PASSWORD" | sudo -S bash ~/arjuna_R2_docker/arjuna_docker_prereq.sh
