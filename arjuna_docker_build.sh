e9eb39320253d5c5d1356093933de8e66389c913
NewrroTechLLP <work@newrro.in>
2025-11-16 13:02:25 +0530

    Update arjuna_docker_build.sh

--- arjuna_docker_build.sh
+++ arjuna_docker_build.sh
@@ -420,7 +420,7 @@ RUN echo "ros2arjuna_open() {" >> /root/.bashrc && \
     echo "    ros2 run nav2_map_server map_saver_cli -f /root/arjuna_ros2/arjuna2_ws/src/arjuna/arjuna/maps/my_map" >> /root/.bashrc && \
     echo "}" >> /root/.bashrc
 
-    echo "source /root/arjuna_ros2/arjuna2_ws/install/setup.bash " >> /root/.bashrc && \
+RUN echo "source /root/arjuna_ros2/arjuna2_ws/install/setup.bash " >> /root/.bashrc && \
 
 
 # Final shell
